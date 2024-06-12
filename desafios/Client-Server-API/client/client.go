4cbadd34ed14b57d54cfb832bd44df24065fe899
Guilherme Felipe Santos <guilherme.f.santos@globalhitss.com.br>
2024-06-12 15:51:08 -0300

    ajuste requisição

--- desafios/Client-Server-API/client/client.go
+++ desafios/Client-Server-API/client/client.go
@@ -1,5 +1,39 @@
 package main
 
-func main(){
+import (
+	"context"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
 
+type StructCotacao struct {
+	USDBRL struct {
+		Bid string `json:"bid"`
+	} `json:"USDBRL"`
+}
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
+	if err != nil {
+		panic(err)
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	file, err := os.Create("cotacao.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	//_, err = file.WriteString(fmt.Sprintf("Dolar: %s"))
+	io.Copy(os.Stdout, res.Body)
+	var cotacao StructCotacao
+	println(cotacao.USDBRL.Bid)
+	//_, err = file.WriteString(fmt.Sprintf("dolar", res.Body))
 }
---
+++ desafios/Client-Server-API/client/go.mod
@@ -0,0 +1,3 @@
+module github.com/zoidepomba/pos-go/blob/main/desafios/Client-Server-API/client
+
+go 1.22.1
