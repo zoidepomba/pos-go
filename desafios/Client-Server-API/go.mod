8f554afc77b7fc5e4d78960308d24f9c796715e4
Guilherme Felipe Santos <guilherme.f.santos@globalhitss.com.br>
2024-06-12 14:01:19 -0300

    criação server

---
+++ desafios/Client-Server-API/go.mod
@@ -0,0 +1,5 @@
+module github.com/zoidepomba/pos-go/tree/main/desafios/Client-Server-API
+
+go 1.22.1
+
+require github.com/mattn/go-sqlite3 v1.14.22
--- desafios/Client-Server-API/server.go
+++ desafios/Client-Server-API/server.go
@@ -2,115 +2,118 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"encoding/json"
-	"io"
 	"log"
 	"net/http"
 	"time"
+	_ "github.com/mattn/go-sqlite3"
+
 )
 
 type StructCotacao struct {
 	USDBRL struct {
-		Code       string `json:"code"`
-		Codein     string `json:"codein"`
-		Name       string `json:"name"`
-		High       string `json:"high"`
-		Low        string `json:"low"`
-		VarBid     string `json:"varBid"`
-		PctChange  string `json:"pctChange"`
-		Bid        string `json:"bid"`
-		Ask        string `json:"ask"`
-		Timestamp  string `json:"timestamp"`
-		CreateDate string `json:"create_date"`
+		Bid string `json:"bid"`
 	} `json:"USDBRL"`
-	EURBRL struct {
-		Code       string `json:"code"`
-		Codein     string `json:"codein"`
-		Name       string `json:"name"`
-		High       string `json:"high"`
-		Low        string `json:"low"`
-		VarBid     string `json:"varBid"`
-		PctChange  string `json:"pctChange"`
-		Bid        string `json:"bid"`
-		Ask        string `json:"ask"`
-		Timestamp  string `json:"timestamp"`
-		CreateDate string `json:"create_date"`
-	} `json:"EURBRL"`
-	BTCBRL struct {
-		Code       string `json:"code"`
-		Codein     string `json:"codein"`
-		Name       string `json:"name"`
-		High       string `json:"high"`
-		Low        string `json:"low"`
-		VarBid     string `json:"varBid"`
-		PctChange  string `json:"pctChange"`
-		Bid        string `json:"bid"`
-		Ask        string `json:"ask"`
-		Timestamp  string `json:"timestamp"`
-		CreateDate string `json:"create_date"`
-	} `json:"BTCBRL"`
 }
 
 func main() {
+
+	criaTabela()
+
+
 	http.HandleFunc("/cotacao", handleCotacao)
 	log.Printf("Servidor rodando na porta :8080\n")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+func criaTabela(){
+	log.Printf("Criando a tabela no banco de dados")
+	db, err := sql.Open("sqlite3", "./cotacoes.db")
+	if err != nil {
+		log.Fatal("Erro ao abrir o banco de dados: %v", err)
+		panic(err)
+	}
+	defer db.Close()
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS cotacoes (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			valor TEXT,
+			data TEXT
+		)
+	`)
+	if err != nil {
+		log.Fatalf("Erro ao criar a tabela: %v", err)
+	}
+}
+
+func execRequest(ctx context.Context) (string, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	if err != nil {
+		return "", err
+	}
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var cotacao StructCotacao
+	if err := json.NewDecoder(resp.Body).Decode(&cotacao); err != nil {
+		return "", err
+	}
+
+	return cotacao.USDBRL.Bid, nil
+}
+
+func saveCotacaoDB(ctx context.Context, db *sql.DB, valor string) error {
+	query := "INSERT INTO cotacoes (valor, data) VALUES (?, datetime('now'))"
+	stmt, err := db.PrepareContext(ctx, query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, valor)
+	return err
+}
+
 func handleCotacao(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	log.Println("Request iniciada")
+
+	ctx, cancel := context.WithTimeout(ctx, 200*time.Millisecond)
+	defer cancel()
 	defer log.Println("Request finalizada")
 
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)	
-	defer cancel()
-
-	req, err := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	req, err := execRequest(ctx)
 	if err != nil {
 		http.Error(w, "Error ao  criar a requisição", http.StatusInternalServerError)
 		log.Println("Erro  ao  criar a requisição", err)
 		return
 	}
 
-	resp, err := http.DefaultClient.Do(req)
+	db, err := sql.Open("sqlite3", "./cotacoes.db")
 	if err != nil {
-		http.Error(w, "Erro  ao fazer  a requisição externa",  http.StatusInternalServerError)
-		log.Println("erro ao   fazer a requisição externa", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	var s StructCotacao
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil{
-		http.Error(w, "Erro ao ler a resposta externa", http.StatusInternalServerError)
-		log.Println("Erro ao ler a resposta externa", err)
+		log.Printf("Erro ao abrir a conexão com o banco de dados: %v", err)
+		http.Error(w, "Erro ao abrir o banco de dados", http.StatusInternalServerError)
 		return
 	}
 
-	if err := json.Unmarshal(body, &s); err != nil {
-		http.Error(w, "Erro ao decodificar a resposta do json", http.StatusInternalServerError)
-		log.Println("Erro ao decodificar resposta JSON:", err)
-		return 
-	}
+	defer db.Close()
 
-	select {
-	case <-ctx.Done():
-		log.Println("Request cancelada pelo client")
-        http.Error(w, "Request cancelada pelo client", http.StatusRequestTimeout)
+	ctxDB, cancelDB := context.WithTimeout(context.Background(), 10 * time.Millisecond)
+	defer cancelDB()
+
+	if err := saveCotacaoDB(ctxDB, db, req); err != nil {
+		http.Error(w, "Erro ao salvar cotação", http.StatusInternalServerError)
+		log.Println("Erro ao salvar cotação: %v", err)
 		return
-	default:
 	}
 
-    w.Header().Set("Content-Type", "application/json")
-    if err := json.NewEncoder(w).Encode(s); err != nil {
-        http.Error(w, "Erro ao codificar resposta JSON", http.StatusInternalServerError)
-        log.Println("Erro ao codificar resposta JSON:", err)
-        return
-    }
-
-    log.Println("Request processada com sucesso")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"bid": req})
+	log.Println("Request processada com sucesso")
 }
-
-
